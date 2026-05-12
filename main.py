58aa3ea6fb445905e3b5c1cd0671ac6c70adb661
MarekAdamira <adamira.marek@gmail.com>
2026-05-12 21:12:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,17 +17,3 @@ with open("./Input/Letters/starting_letter.txt", "r") as starting_letter:
                 ready_to_send.write(personal_letter)
 
 
-
-
-
-
-
-
-
-
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
